2f7c5b0ef4edd0d2bc01526901b25218ffee44c6
beez0mbie <vzhik3268@gmail.com>
2023-08-28 17:21:10 +0300

    use localhost

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://api.mesto.ashmelkov.nomoredomainsicu.ru';
+// export const BASE_URL = 'https://api.mesto.ashmelkov.nomoredomainsicu.ru';
+export const BASE_URL = 'http://localhost:3000';
